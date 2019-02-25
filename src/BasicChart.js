30814b171aa0fae2cb070408511c45339e6d9754
Alex Warnes <alex.warnes@moovel.com>
2019-02-24 17:21:29 -0800

    First POC for chart with tasks.

--- src/BasicChart.js
+++ src/BasicChart.js
@@ -3,7 +3,11 @@ import { Bar } from 'react-chartjs-2';
 import {
   Paper,
   withStyles,
-  TextField
+  TextField,
+  Button,
+  Divider,
+  Switch,
+  FormControlLabel
 } from '@material-ui/core'
 import moment from 'moment-timezone'
 
@@ -21,10 +25,17 @@ const styles = {
   }
 }
 
+const timeBucketFilterArray = ["Day", "Week", "Month", "Year"]
+
 class BasicChart extends Component {
   state = {
     taskTypes: [],
-    taskEvents: []
+    taskEvents: [],
+    activeTaskTypes: [],
+    filterByTaskTypes: true,
+    activeBucketFilter: "Week",
+    startTimeInterval: moment().startOf("day").valueOf(),
+    endTimeInterval: moment().endOf("day").valueOf()
   }
 
   componentDidMount () {
@@ -41,26 +52,106 @@ class BasicChart extends Component {
   setTaskEvents = () => {
     const { userData } = this.props
     const taskEvents = Object.values(userData.taskEvents)
-    console.error("XKCD: ", taskEvents)
     this.setState({ taskEvents })
   }
 
+  updateActiveTaskTypes = (taskType) => {
+    const { activeTaskTypes } = this.state
+    if (activeTaskTypes.includes(taskType)) {
+      const newActiveTaskTypes = activeTaskTypes.filter(type => type !== taskType)
+      this.setState({activeTaskTypes: newActiveTaskTypes})
+    } else {
+      const newActiveTaskTypes = activeTaskTypes.concat(taskType)
+      this.setState({activeTaskTypes: newActiveTaskTypes})
+    }
+  }
+
+  updateActiveBucketFilter = (bucket) => {
+    this.setState({
+      activeBucketFilter: bucket,
+      filterByTaskTypes: false
+    })
+  }
+
+  handleTimeIntervalChange = (name, evt) => {
+    let time
+    if (name === "startTimeInterval") {
+      time = moment(evt.target.value).startOf("day").valueOf()
+    } else {
+      time = moment(evt.target.value).endOf("day").valueOf()
+    }
+    this.setState({[name]: time })
+  }
+
   render() {
-    const { userData, classes } = this.props
+    const { classes } = this.props
+    const {
+      taskTypes,
+      taskEvents: eventsForTasks,
+      activeTaskTypes,
+      activeBucketFilter,
+      filterByTaskTypes,
+      startTimeInterval,
+      endTimeInterval
+    } = this.state
+
+    let taskEvents = eventsForTasks;
+
+    if (startTimeInterval && endTimeInterval) {
+      taskEvents = eventsForTasks.filter(event => (event.startTime >= startTimeInterval) && (event.startTime <= endTimeInterval))
+    }
+
+    const activeEventTotals = taskEvents.reduce((total, event) => {
+      if (!activeTaskTypes.includes(event.type)) return total;
+      if (total[event.type]) {
+        total[event.type] += event.endTime - event.startTime
+      } else {
+        total[event.type] = event.endTime - event.startTime
+      }
+      return total
+    }, [])
+    
+    const displayData = activeTaskTypes.map(type => activeEventTotals[type] / 60000)
+
     const data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: filterByTaskTypes ? activeTaskTypes : [],
       datasets: [
         {
-          label: `Time by ${"bucketSelection.name"}`,
+          label: `Filtered by ${filterByTaskTypes ? "Task Types" : activeBucketFilter}`,
           backgroundColor: 'rgba(255,99,132,0.2)',
           borderColor: 'rgba(255,99,132,1)',
           borderWidth: 1,
           hoverBackgroundColor: 'rgba(255,99,132,0.4)',
           hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [65, 59, 80, 0, 56, 55, 40]
+          data: displayData || []
         }
       ]
     };
+
+    const taskFilters = taskTypes.map(taskType => {
+      return (
+        <Button
+          key={`${taskType}-filterButton`}
+          onClick={() => { this.updateActiveTaskTypes(taskType) }}
+          variant="contained"
+          color={activeTaskTypes.includes(taskType) ? "primary" : "default"}>
+          {taskType}
+        </Button>
+      )
+    })
+
+    const timeBucketFilters = timeBucketFilterArray.map(bucketType => {
+      return (
+        <Button
+          key={`${bucketType}-button`}
+          onClick={() => { this.updateActiveBucketFilter(bucketType) }}
+          variant="contained"
+          color={(bucketType === activeBucketFilter) && !filterByTaskTypes ? "primary" : "default"}> 
+          {bucketType}
+        </Button>
+      )
+    })
+
     return (
       <Paper className={classes.paper}>
         <div className={classes.datePickerContainer}>
@@ -71,7 +162,10 @@ class BasicChart extends Component {
             InputLabelProps={{
               shrink: true,
             }}
+            error={endTimeInterval < startTimeInterval}
             className={classes.datePicker}
+            value={moment(startTimeInterval).format("YYYY-MM-DD") || ""}
+            onChange={(evt) => {this.handleTimeIntervalChange("startTimeInterval", evt)}}
           />
           <TextField
             id="endDate"
@@ -80,12 +174,35 @@ class BasicChart extends Component {
             InputLabelProps={{
               shrink: true,
             }}
+            error={endTimeInterval < startTimeInterval}
             className={classes.datePicker}
+            value={moment(endTimeInterval).format("YYYY-MM-DD") || ""}
+            onChange={(evt) => {this.handleTimeIntervalChange("endTimeInterval", evt)}}
           />
         </div>
+        <Divider />
         <Bar
           data={data}
         />
+        <Divider />
+        <div className={classes.filterContainer}>
+          <div>
+            <FormControlLabel
+              control={<Switch
+                onChange={() => { this.setState({filterByTaskTypes: !filterByTaskTypes})}}
+                checked={filterByTaskTypes}
+                value={filterByTaskTypes}
+              />}
+            label="Filter by Task Type"
+            />
+            <br />
+            { taskFilters }
+          </div>
+          <Divider />
+          <div>
+            { timeBucketFilters }
+          </div>
+        </div>
       </Paper>
     )
   }
